b7468b987b9e88dc7013d9aeaaef3ce533f0ec6c
Udit Negi <uditnegi27@gmail.com>
2023-02-22 17:10:32 +0530

    checkout flow with coupon

--- src/main/java/com/mamaearth/PageObject/mamaearth_CartPage.java
+++ src/main/java/com/mamaearth/PageObject/mamaearth_CartPage.java
@@ -1,6 +1,5 @@
 package com.mamaearth.PageObject;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,9 +18,28 @@ public class mamaearth_CartPage extends mamaearth_Base{
 	@FindBy(xpath = "//button[contains(text(), 'Continue')]")
 	WebElement continue_button;
 	
+	@FindBy(xpath = "//div[contains(@class, 'OrderSummary__CouponTrayStrip-sc-b29dda-0 edRxHm')]")
+	WebElement applyCoupon_button;
+	
+	@FindBy(xpath = "//input[contains(@class, 'CouponTrayVariant__StyledInput-sc-1hws438-0 jfrbVq')]")
+	WebElement coupon_button;
+	
+	@FindBy(xpath = "//input[contains(@class, 'CouponTrayVariant__StyledInput-sc-1hws438-0 jfrbVq')]//following-sibling::div")
+	WebElement apply_button;
+	
 	public mamaearth_HomePage click_on_mamaearth_logo() {
 		mamaearth_logo.click();
 		return new mamaearth_HomePage();
+	}
+	
+	public void click_on_applyCoupon() {
+		applyCoupon_button.click();
+	}
+	
+	public void apply_coupon() {
+		coupon_button.click();
+		coupon_button.sendKeys("REDEEM15");
+		apply_button.click();
 	}
 	
 	public mamaearth_CheckoutPage continue_to_checkout() {
