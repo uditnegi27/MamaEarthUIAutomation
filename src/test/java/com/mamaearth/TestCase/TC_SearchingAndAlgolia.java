7dc9f6f7216ef3a9f62e1ac8a4eafcb1c4c8cb77
Udit Negi <uditnegi27@gmail.com>
2023-04-06 14:12:45 +0530

    update in pom xml file

--- src/test/java/com/mamaearth/TestCase/TC_SearchingAndAlgolia.java
+++ src/test/java/com/mamaearth/TestCase/TC_SearchingAndAlgolia.java
@@ -6,24 +6,19 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 
 import com.mamaearth.Base.mamaearth_Base;
-import com.mamaearth.PageObject.mamaearth_HomePage;
 import com.mamaearth.Utils.mamaearth_utils;
 import com.mamaearth.Utils.mamaearth_utils_Algolia;
 
 public class TC_SearchingAndAlgolia extends mamaearth_Base{
 	
-	mamaearth_HomePage home;
-	
-	
-	
+
 	@BeforeMethod
 	public void setup() {
 		initialization();
-		
 	}
 	
 	@Test
-	public void Test_Search_ProductSearch() {
+	public void Test_mamaearth_Search_ProductSearch() {
 		mamaearth_utils_Algolia.search_product_category(prop.getProperty("search_categoryName"));
 		mamaearth_utils.take_screenShot("test");
 		Assert.assertTrue(mamaearth_utils_Algolia.verify_searched_product_category(prop.getProperty("search_categoryName")), "Products are not matching with the searched cateogry:  -->  ");
